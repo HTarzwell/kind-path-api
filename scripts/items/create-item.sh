f6b114e5a256ebaaf2ef108db2c16816642bd80e
HTarzwell <htarzwell@gmail.com>
2018-04-15 13:35:28 -0400

    OpenReadController & new item curls
    -Replaced ApplicationController with OpenReadController in items model
    -Created new shell file for curls and started create-item shell command

---
+++ scripts/items/create-item.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+API="${API_ORIGIN:-http://localhost:4741}"
+URL_PATH="/items"
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=$TOKEN" \
+  --data '{
+    "items": {
+      "date": "'"${DATE}"'",
+      "description": "'"${DESCRIPTION}"'",
+      "energy_needed": "'"${ENERGY}"'",
+      "is_completed": "'"${COMPLETED}"'"
+      "is_crucial": "'"${CRUCIAL}"'"
+    }
+  }'
+
+echo
