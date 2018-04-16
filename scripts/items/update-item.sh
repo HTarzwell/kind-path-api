6b47c87fbb28829a1d2ec6fb233479e60b48641a
HTarzwell <htarzwell@gmail.com>
2018-04-16 09:59:48 -0400

    Added CURL scripts for all actions
    -Successfully CRUDed on main resource

--- scripts/items/update-item.sh
+++ scripts/items/update-item.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 API="${API_ORIGIN:-http://localhost:4741}"
-URL_PATH="/items"
+URL_PATH="/items/${ID}"
 curl "${API}${URL_PATH}" \
   --include \
-  --request POST \
+  --request PATCH \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=$TOKEN" \
   --data '{
